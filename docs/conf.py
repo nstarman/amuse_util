462b3b57da9c3b3de39469192f35924f59200d74
Nathaniel Starkman <nstarkman@protonmail.com>
2020-02-09 15:46:49 -0500

    customize docs
    get rid of astropy link
    
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -135,6 +135,10 @@ html_title = '{0} v{1}'.format(project, release)
 # Output file base name for HTML help builder.
 htmlhelp_basename = project + 'doc'
 
+# Static files to copy after template files
+html_static_path = ['_static']
+html_style = 'amuse_utils.css'
+
 
 # -- Options for LaTeX output -------------------------------------------------
 
