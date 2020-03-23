cdbc64f5648ddf5c3a51e8223c746fc4f284e09f
Nathaniel Starkman <nstarkman@protonmail.com>
2020-03-22 21:40:35 -0400

    add logging backup when can’t import astroPHD
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

---
+++ amuse_utils/utils/_logging/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : logging initialization file
+# AUTHOR  : Nathaniel Starkman
+#
+# ----------------------------------------------------------------------------
+
+# Docstring and Metadata
+"""initialization file for logging."""
+
+__author__ = "Nathaniel Starkman"
+
+
+##############################################################################
+# IMPORTS
+
+# GENERAL
+import logging
+
+# PROJECT-SPECIFIC
+from ._LogPrint import LogPrint
+from ._LogFile import LogFile
+
+
+##############################################################################
+# END
